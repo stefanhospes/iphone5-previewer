f641d8f22f752d8223a840a9b86ada539bd304ce
Stefan Hospes <shospes@gmail.com>
2013-04-12 14:52:06 +0200

    Fixed post bug

--- js/ext.js
+++ js/ext.js
@@ -3,7 +3,7 @@ $(document).ready(function() {
 		var url = $('input[name=url]').val();
 		
 		if(url.length > 2) {
-			$('.iframe').attr('src', 'http://' + url.replace('http://',''));
+			$('.iframe').attr('src', 'http://' + url.replace('http://','').replace('https://',''));
 		}
 		
 		return false;
